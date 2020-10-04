6c4e73fc03ce1aaaf17ec26133d2c979d13475b1
Mateusz Stańczak <mateusz.stanczak@pgf.com.pl>
2020-10-04 09:26:12 +0100

    feat(toolbar): working toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StartComponent } from './component/start/start.component';
-import { SelectCityComponent } from './component/select-city/select-city.component';
-import { ShowWeatherComponent } from './component/show-weather/show-weather.component';
-import { ToolbarComponent } from './component/toolbar/toolbar.component';
-import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StartComponent} from './component/start/start.component';
+import {SelectCityComponent} from './component/select-city/select-city.component';
+import {ShowWeatherComponent} from './component/show-weather/show-weather.component';
+import {ToolbarComponent} from './component/toolbar/toolbar.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+];
+
 
 @NgModule({
   declarations: [
@@ -20,9 +31,12 @@ import { PageNotFoundComponent } from './component/page-not-found/page-not-found
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
